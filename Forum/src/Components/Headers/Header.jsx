a1dd5a110b92f0afeefdce11de1207fa94d21140
Masterclass777 <borislav.2404g@gmail.com>
2025-05-08 19:28:15 +0300

    logout function wass added in header file

--- Forum/src/Components/Headers/Header.jsx
+++ Forum/src/Components/Headers/Header.jsx
@@ -1,12 +1,33 @@
 import { NavLink  } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../../state/app.context";
 function Header() {
+
+    const { user, userData, setAppState } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        logoutUser()
+            .then(() => {
+                setAppState({
+                    user: null,
+                    userData: null
+                });
+                navigate('/login');
+            })
+            .catch((error) => {
+                console.error("Logout failed:", error);
+            });
+    }
     return (
         <header>
             <nav>
                 <NavLink to='/login'>Login</NavLink>
                 <NavLink to='/register'>Register</NavLink>
             </nav>
+            {user && <button onClick={logout}>Logout</button>}
+            {userData && <span>Welcome, {userData.handle}</span>}
         </header>
     )
 }
