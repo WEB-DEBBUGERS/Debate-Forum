47e01f9c11c61167ef04f5d0ae04b06f0a092ae6
gamebred97 <krasiikostdainov7@gmail.com>
2025-05-09 07:49:20 +0300

    Adding conditional rendering to visualize home page logic

--- Forum/src/views/Home/Home.jsx
+++ Forum/src/views/Home/Home.jsx
@@ -1,9 +1,14 @@
 import NewsFeed from "./News/NewsFeed"
+import { useContext } from "react"
+import { AppContext } from "../../state/app.context"
 
 export default function Home() {
+
+    const { user } = useContext(AppContext);
+
     return (
         <div className="home">
-            <NewsFeed/>
+            {!user ? <NewsFeed/> : <h1>Here will be the logged in user component Home page</h1>}
         </div>
     )
 }
