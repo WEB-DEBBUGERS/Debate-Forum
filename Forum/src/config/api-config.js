dc233af1a3a4f384c2241cd920425cfb6d05329a
Masterclass777 <borislav.2404g@gmail.com>
2025-05-23 13:28:47 +0300

    config.file

--- Forum/src/config/api-config.js
+++ Forum/src/config/api-config.js
@@ -1,2 +1,2 @@
 export const API_KEY = import.meta.env.VITE_API_KEY;
-export const NEWS_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+export const NEWS_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
